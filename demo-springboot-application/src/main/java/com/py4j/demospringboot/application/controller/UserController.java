eb234513d6ab537869295156137acc1a123c97d8
warrenpu <1210513673@qq.com>
2018-05-18 16:16:45 -0400

    add CRUD of user controller

--- demo-springboot-application/src/main/java/com/py4j/demospringboot/application/controller/UserController.java
+++ demo-springboot-application/src/main/java/com/py4j/demospringboot/application/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.py4j.demospringboot.application.controller;
 
 import com.py4j.demospringboot.application.CustomerRepository;
+import com.py4j.demospringboot.application.po.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final Logger mLogger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private CustomerRepository customerRepository;
@@ -16,4 +23,53 @@ public class UserController {
         return customerRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Object getCustomerById (@PathVariable String id) {
+        return customerRepository.findById(Long.valueOf(id));
+    }
+
+    @PostMapping("/{name}/{email}")
+    public Object addCustomer (@PathVariable String name,@PathVariable String email) {
+        Customer c = new Customer();
+        c.setName(name);
+        c.setEmail(email);
+        try {
+            customerRepository.save(c);
+        } catch (Exception e) {
+            mLogger.error("",e);
+            return false;
+        }
+        return true;
+    }
+
+    @DeleteMapping("/{id}")
+    public Object delCustomer (@PathVariable String id) {
+        try {
+            customerRepository.deleteById(Long.valueOf(id));
+        } catch (Exception e) {
+            mLogger.error("",e);
+            return false;
+        }
+        return true;
+    }
+
+    @PutMapping("/{id}/{name}/{email}")
+    public Object updateCustomer (@PathVariable String id,@PathVariable String name,@PathVariable String email) {
+        Optional<Customer> optional = customerRepository.findById(Long.valueOf(id));
+        if (!optional.isPresent()) {
+            mLogger.warn("[UserController] [updateCustomer] => no customer found by id {}",id);
+            return false;
+        }
+        Customer customer = optional.get();
+        customer.setName(name);
+        customer.setEmail(email);
+        customerRepository.save(customer);
+        try {
+            customerRepository.save(customer);
+        } catch (Exception e) {
+            mLogger.error("",e);
+            return false;
+        }
+        return true;
+    }
 }
--- demo-springboot-application/src/main/java/com/py4j/demospringboot/application/po/Customer.java
+++ demo-springboot-application/src/main/java/com/py4j/demospringboot/application/po/Customer.java
@@ -10,17 +10,17 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private long id;
 
     private String name;
 
     private String email;
 
-    public Integer getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(long id) {
         this.id = id;
     }
 
