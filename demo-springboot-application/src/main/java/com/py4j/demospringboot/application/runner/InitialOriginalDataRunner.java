572d711acb921aaaa5e6a9fc22a8087fffdd69df
warrenpu <1210513673@qq.com>
2018-05-24 22:17:28 -0400

    make InitialOriginalDataRunner not add book data

--- demo-springboot-application/src/main/java/com/py4j/demospringboot/application/runner/InitialOriginalDataRunner.java
+++ demo-springboot-application/src/main/java/com/py4j/demospringboot/application/runner/InitialOriginalDataRunner.java
@@ -1,6 +1,5 @@
 package com.py4j.demospringboot.application.runner;
 
-import com.py4j.demospringboot.application.po.Book;
 import com.py4j.demospringboot.application.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,9 +18,9 @@ public class InitialOriginalDataRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        bookService.save(new Book("三国演义","罗贯中","描述了东汉末年乱世割据的故事"));
-        bookService.save(new Book("西游记","吴承恩","一段师徒四人西天取经，战胜妖魔鬼怪的故事"));
-        bookService.save(new Book("水浒传","施耐庵","宋朝的一群梁山好汉的故事，其实就是一群土匪"));
+        //bookService.save(new Book("三国演义","罗贯中","描述了东汉末年乱世割据的故事"));
+        //bookService.save(new Book("西游记","吴承恩","一段师徒四人西天取经，战胜妖魔鬼怪的故事"));
+        //bookService.save(new Book("水浒传","施耐庵","宋朝的一群梁山好汉的故事，其实就是一群土匪"));
     }
 
 }
